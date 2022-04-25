191c58ee83cea0d95a0c70da1172b15f7aea3279
Moburu <stevenschiffner3@gmail.com>
2022-04-25 18:20:48 -0400

    Added a loading animation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,19 +2,19 @@ import './Card.css';
 import React from 'react'
 import PropTypes from 'prop-types'
 import fromNow from 'from-now';
+import { Placeholder, PlaceholderImage } from 'semantic-ui-react';
 
 const Card = props => {
   return (
-
-    <div className='Card'>
-      <h2 className='post-title'>{props.title}</h2>
-      <img className='post-thumbnail' src={props.thumbnail} alt={'Reddit thumbnail'} />
-      <div className='post-info'>
-        <span className='attribution'>Posted by {props.author}</span>
-        <span className='timestamp'>{fromNow(props.created*1000)+' ago'}</span>
-        <span className='comments'>{props.num_comments+' comments'}</span>
-      </div>
-    </div>
+        <div className='Card'>
+            <h2 className='post-title'>{props.title}</h2>
+            <img className='post-thumbnail' src={props.thumbnail} alt={'Reddit thumbnail'} />
+            <div className='post-info'>
+              <span className='attribution'>Posted by {props.author}</span>
+              <span className='timestamp'>{fromNow(props.created*1000)+' ago'}</span>
+              <span className='comments'>{props.num_comments+' comments'}</span>
+            </div>
+        </div>
   )
 }
 
--- src/features/RedditData/RedditData.js
+++ src/features/RedditData/RedditData.js
@@ -3,27 +3,30 @@ import PropTypes from 'prop-types'
 import Card from '../../components/Card/Card';
 import './RedditData.css'
 import { useSelector, useDispatch } from 'react-redux';
-import { selectPosts } from './redditDataSlice';
-
+import { selectPosts, selectIsLoading } from './redditDataSlice';
+import { Loader } from 'semantic-ui-react';
 
 
 const RedditData = props => {
   const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
+  const isLoading = useSelector(selectIsLoading);
 
   return (
     <div className='RedditData'>
-        {posts.map((post) => (
+        {isLoading === true ? <Loader active inline='centered' /> :
+          posts.map((post) => (
             <Card
                 title={post['data'].title}
-                thumbnail={post['data'].thumbnail === 'self' || post['data'].thumbnail === 'default' ? 'https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png' : post['data'].thumbnail}
+                thumbnail={post['data'].thumbnail === 'self' || post['data'].thumbnail === 'default' || post['data'].thumbnail === 'image' || post['data'].thumbnail === 'spoiler' ? 'https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png' : post['data'].thumbnail}
                 score={post['data'].score}
                 author={post['data'].author}
                 created={post['data'].created}
                 num_comments={post['data'].num_comments}
                 url={post['data'].url}
             />
-        ))}
+          ))
+        }
     </div>
   )
 }
