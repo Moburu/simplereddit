d9507c1a451a33e689f55a10792cf0bb58a817c6
Moburu <stevenschiffner3@gmail.com>
2022-05-04 14:50:30 -0400

    Added back to top button

--- src/app/App.js
+++ src/app/App.js
@@ -6,6 +6,7 @@ import RedditData from '../features/RedditData/RedditData';
 import 'fomantic-ui-css/semantic.min.css';
 import { loadPopular } from '../features/RedditData/redditDataSlice';
 import { useDispatch } from 'react-redux';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   const dispatch = useDispatch();
@@ -16,8 +17,10 @@ function App() {
 
   return (
     <div className="App">
-      <Search />
-      <RedditData />
+      <div className='background'></div>
+      <div className='button-holder'><a href='#Header' className='button-link'><Button className='back-to-top'>Back to top</Button></a></div>
+      <Search className='data'/>
+      <RedditData className='data'/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import './Header.css';
 import React from 'react';
 import logo from './logo.png';
+import { Button } from 'semantic-ui-react';
 
 const Header = props => {
   return (
